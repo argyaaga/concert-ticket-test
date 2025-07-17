d39565217022c048edbfc6bc103c3c37ef2df71c
argyaaga <argyaschool@gmail.com>
2025-07-17 18:20:21 +0800

    Initial setup

---
+++ src/main/java/com/ticketing/ticket_reserve/TicketReserveApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.ticket_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketReserveApplication.class, args);
+	}
+
+}
